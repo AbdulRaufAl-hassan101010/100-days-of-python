22da76a7bd85ae779a793b69550d44463ea68fde
AbdulRaufAl-hassan101010 <abdulraufalhassan101010@gmail.com>
2023-02-19 12:55:40 +0000

    learn about dataframes and series

--- 25-data/cvs_pandas.py
+++ 25-data/cvs_pandas.py
@@ -22,6 +22,41 @@
 
 
 import pandas
-# read file
+"""
+Data types in pandas
+Dataframe refers to the whole spreadsheet
+series refers to columns
+"""
+# read cvs file
 data = pandas.read_csv("./002 weather-data.csv")
-print(data["temp"])
+
+data_dict = data.to_dict()
+
+data_list = data["temp"].to_list()
+# print(data_list)
+
+# get average temperature
+average = sum(data_list) / len(data_list)
+average = data['temp'].mean()
+
+
+# get max temperature row
+maximum_value = data.temp.max()
+# print(data[data.temp == maximum_value])
+
+# get temperature for for monday
+monday = data[data.day == 'Monday']
+monday_temp = int(monday.temp)
+monday_temp_F = monday.temp * 9/5 + 32
+# print(monday_temp_F)
+
+
+# create a dataframe from scratch
+data_dict = {
+    "students": ["Kelvin", "Karen", "Jay"],
+    "scores": [90, 34, 23]
+}
+
+data = pandas.DataFrame(data_dict)
+data.to_csv("new_data.csv")
+print(data)
